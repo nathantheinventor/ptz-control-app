35567c26a6d47e801cc785a0b47d59229054f280
Nathan Collins <nathantheinventor@gmail.com>
2024-03-18 17:17:49 -0400

    feat: Create db models
    Addresses #1

--- ptz_app/models.py
+++ ptz_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class CameraSettings(models.Model):
+    """Default camera settings."""
+    aperture = models.FloatField(null=True)
+    brightness = models.IntegerField(null=True)
+    saturation = models.IntegerField(null=True)
+    contrast = models.IntegerField(null=True)
+    sharpness = models.IntegerField(null=True)
+    hue = models.IntegerField(null=True)
+
+    # TODO: Find all camera settings
+
+
+class Camera(models.Model):
+    """Camera model."""
+    name = models.CharField(max_length=100)
+    ip = models.GenericIPAddressField()
+    username = models.CharField(max_length=100, null=True)
+    password = models.CharField(max_length=100, null=True)
+    default_settings = models.ForeignKey(CameraSettings, on_delete=models.CASCADE)
+
+
+class Preset(models.Model):
+    """Camera preset model."""
+    name = models.CharField(max_length=100)
+    order = models.IntegerField()
+    thumbnail = models.ImageField()
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+    settings = models.ForeignKey(CameraSettings, on_delete=models.CASCADE)
+    pan = models.FloatField()
+    tilt = models.FloatField()
+    zoom = models.FloatField()
+    focus = models.FloatField()
