15ea492ab4c4f9ddbe70f76ce8eafd6ca2e1a5f5
Nathan Collins <nathantheinventor@gmail.com>
2024-03-19 09:55:18 -0400

    chore: Add types to views

--- ptz_app/views/index.py
+++ ptz_app/views/index.py
@@ -1,11 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """Render the index page"""
     return render(request, "ptz_app/index.html")
 
 
-def recall_preset(request):
+def recall_preset(request: HttpRequest):
     """Recall a given preset"""
     ...  # TODO: implement
