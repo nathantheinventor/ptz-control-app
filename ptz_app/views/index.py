ced7d154eeff932f157e002627736dbc8d424e7a
Nathan Collins <nathantheinventor@gmail.com>
2024-03-19 10:08:30 -0400

    debug: Figure out how to pass context and turn off Pyright

--- ptz_app/views/index.py
+++ ptz_app/views/index.py
@@ -1,10 +1,17 @@
+import json
+
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
     """Render the index page"""
-    return render(request, "ptz_app/index.html")
+    from ptz_app.models import Camera
+
+    context = {
+        "cameras": json.dumps([camera.json() for camera in Camera.objects.all()])
+    }
+    return render(request, "ptz_app/index.html", context)
 
 
 def recall_preset(request: HttpRequest, preset_id: int) -> JsonResponse:
