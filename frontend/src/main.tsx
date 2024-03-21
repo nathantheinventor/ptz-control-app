b731f6a33e232ac34c3d64a932bfc0cfba46b08c
Nathan Collins <nathantheinventor@gmail.com>
2024-03-21 14:25:53 -0400

    feat: Set up React frontend to work with Django backend
    Closes #2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { CameraList } from "./pages/CameraList";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const root = document.getElementById("root");
+const cameraList = document.getElementById("camera-list");
+// const cameraEditor = document.getElementById("camera-editor")
+// const presetEditor = document.getElementById("preset-editor")
+
+const data = document.getElementById("data") as HTMLInputElement;
+
+const pages: [HTMLElement | null, () => JSX.Element][] = [
+  [root, () => <App />],
+  [cameraList, () => <CameraList cameras={JSON.parse(data.value)} />],
+  // [cameraEditor, () => <CameraEditor camera={JSON.parse(data.value)} />],
+  // [presetEditor, () => <PresetEditor preset={JSON.parse(data.value)} />]
+];
+
+for (const [element, component] of pages) {
+  if (element) {
+    ReactDOM.createRoot(element).render(
+      <React.StrictMode>{component()}</React.StrictMode>,
+    );
+    break;
+  }
+}
