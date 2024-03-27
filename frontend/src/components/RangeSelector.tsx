8372969818b6b592eb2dfd95128b8c840bb8985d
Nathan Collins <nathantheinventor@gmail.com>
2024-03-27 14:27:27 -0400

    feat: Make range selector wider and add plus / minus buttons

--- frontend/src/components/RangeSelector.tsx
+++ frontend/src/components/RangeSelector.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 type RangeSelectorProps = {
   label: string;
   value: number | null | undefined;
@@ -30,16 +33,26 @@ export function RangeSelector({
   return (
     <div className='flex flex-col pb-2'>
       <span className='text-sm text-gray-500'>{label}</span>
-      <div className='flex align-center'>
+      <div className='flex align-center items-center'>
         {!notNull && <input type='checkbox' checked={!disabled} onChange={toggleDisabled} className='mr-2' />}
+        <FontAwesomeIcon
+          className='cursor-pointer'
+          icon={faMinus}
+          onClick={value != null ? () => onChange(Math.max(min, value - 1)) : undefined}
+        />
         <input
           type='range'
           value={value ?? min}
           disabled={!notNull && disabled}
           onChange={(e) => onChange(parseInt(e.target.value))}
-          className='border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-150 ease-in-out w-80'
+          className='border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-150 ease-in-out !w-[800px] !mx-2'
           min={min}
           max={max}
+        />
+        <FontAwesomeIcon
+          className='cursor-pointer'
+          icon={faPlus}
+          onClick={value != null ? () => onChange(Math.min(max, value + 1)) : undefined}
         />
         <span className={`ml-2 cursor-pointer ${disabled ? 'text-gray-500' : ''}`} onClick={promptValue}>
           {value != null ? `${value}${postfix}` : 'unset'}
